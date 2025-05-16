7b6f16a6d8f54ce51eab2b702110803198ee986b
Kerem Erkan <kerem.erkan@gmail.com>
2025-05-16 08:33:41 +0300

    Added umbrella header and removed tests

---
+++ qrencode-umbrella.h
@@ -0,0 +1,18 @@
+//
+//  qrencode-umbrella.h
+//  qrencode
+//
+//  Created by Kerem Erkan on 12.03.2023.
+//
+
+#include <stdio.h>
+#import "qrencode.h"
+#import "qrencode_inner.h"
+#import "qrinput.h"
+#import "qrspec.h"
+#import "mqrspec.h"
+#import "bitstream.h"
+#import "mask.h"
+#import "mmask.h"
+#import "split.h"
+#import "rsecc.h"
